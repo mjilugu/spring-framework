c3df87d74acbd4dc679d1dc3d9039b5d7cf18375
emosjil <moses.jilugu@ericsson.com>
2014-01-22 14:45:51 +0000

    Sample projects for practice

---
+++ gs-rest-service/src/main/java/hello/GreetingController.java
@@ -0,0 +1,23 @@
+package hello;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class GreetingController {
+
+    private static final String template = "%s, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public @ResponseBody Greeting greeting(
+            @RequestParam(value="name", required=false, defaultValue="World") String name,
+			@RequestParam(value="greeting", required=true) String greeting) {
+		System.out.println("Responding to Query.!");
+        return new Greeting(counter.incrementAndGet(),
+                            String.format(template, greeting, name));
+    }
+}
